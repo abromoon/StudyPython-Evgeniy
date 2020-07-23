2b24f250ef24e50f9c168ccae947a8c4a3d2eec3
Evgeniy <lipov_y@mail.ru>
2020-07-23 15:25:38 +0300

    ✅

---
+++ Task_3.4_2.py
@@ -0,0 +1,28 @@
+# https://stepik.org/lesson/3363/step/2?unit=1135
+
+"""
+Напишите программу, которая считывает из файла строку, соответствующую тексту,
+сжатому с помощью кодирования повторов, и производит обратную операцию, получая исходный текст.
+"""
+
+t = ''
+n = ''
+with open(r'dataset_3363_2 (5).txt', 'r') as file:
+    line = file.readline().strip()
+
+with open('answer.txt', 'w') as file:
+    print(line)
+
+    for ch in line:
+        if ch.isalpha():
+            if t != '':
+                file.write(t * int(n))
+                n = ''
+                t = ch
+            else:
+                t = ch
+
+        else:
+            n += ch
+    file.write(t * int(n))
+
